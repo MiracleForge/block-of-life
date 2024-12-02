a9743ae3532184655adea36b0fdf9303f93d7180
paulodev <paulomoreirarosado@hotmail.com>
2024-12-02 01:54:59 -0300

    Feat(main.c): Criado um array unidimencional para armazenar os dados das celulas vivas, sou capaz de desenhar um celula

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
+#include <stdlib.h>
+#ifdef _WIN32
+#include <conio.h>
+#else
 #include <stdio.h>
+#include <unistd.h>
+#define clrscr() printf("\e[1;1H\e[2J")
+#endif
 
-#define ROW_NUMBER 29
-#define COLLUNS_NUMBER 10
+#define ROW_NUMBER 30
+#define COLUMNS_NUMBER 10
 
 int Draw_Tittle(char* title) {
     printf("\33[1;33m");
@@ -10,14 +17,14 @@ int Draw_Tittle(char* title) {
     return 0;
 }
 
-int Draw_Grid (int row_number, int collums_number, int alive_cell) {
+int Draw_Grid (int row_number, int collums_number, int *cellmap) {
   int _cell_position = 0;
 
      for (int y = 0; y < collums_number; y++) {
          for (int i = 0; i < row_number; i++) {
            _cell_position++;
            
-             if (alive_cell == _cell_position) {
+             if (cellmap[_cell_position -1] == 1) {
                 printf("[█]");                
              } else {
                 printf("[ ]");
@@ -28,11 +35,48 @@ int Draw_Grid (int row_number, int collums_number, int alive_cell) {
     return 0;
 }
 
- 
+void update(int *cell_map) {
+    for (int i = 0; i < ROW_NUMBER * COLUMNS_NUMBER; i++) {
+        if (cell_map[i] == 1) {
+            cell_map[i] = 0;
+        }  
+     }
+}
 
 int main () {
+    int *cell_map = malloc (ROW_NUMBER * COLUMNS_NUMBER * sizeof(int));
+    int game_running = 1;
+    int exit;
+    int interation = 0;
+    int row  = 15;
+    int columns = 5;
+    int start_point = (row * COLUMNS_NUMBER) + columns;
+    char user_input[10];
+
+    printf("Inter a integer: \n");
+    
+    fgets(user_input, sizeof(user_input), stdin);
+    int user_value = atoi(user_input);
+    printf("user input : %d\n", user_value); 
+    if (cell_map == NULL) {
+        printf("Fail to alocate memory!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < ROW_NUMBER * COLUMNS_NUMBER; i++) {
+        cell_map[i] = 0;        
+    }
+    cell_map[start_point] = 1;
 
     Draw_Tittle("Welcome to Game of Life");
-    Draw_Grid(ROW_NUMBER, COLLUNS_NUMBER, 8);
-    return 1;
-}
+   // scanf("Press q to cancel %d", &exit);
+    while (game_running) {
+      clrscr();  
+      Draw_Grid(ROW_NUMBER, COLUMNS_NUMBER, cell_map);
+      sleep(1);
+      update(cell_map);    
+ }
+        
+    free(cell_map);
+    return 0;   
+   }
